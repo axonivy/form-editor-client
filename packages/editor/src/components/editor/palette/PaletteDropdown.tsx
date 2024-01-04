8997341d197b070d259ddee1fad1f03e6e7f87c2
Fabian Heuberger <fabian.heuberger@axonivy.com>
2024-01-04 16:07:38 +0100

    XIVY-13128 WIP added TODO

--- packages/editor/src/components/editor/palette/PaletteDropdown.tsx
+++ packages/editor/src/components/editor/palette/PaletteDropdown.tsx
@@ -2,11 +2,19 @@ import './PaletteDropdown.css';
 import type { PaletteDetails } from './Palette';
 import { PaletteItem } from './PaletteItem';
 
+// TODO: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown
+
 export const PaletteDropdown = (category: PaletteDetails) => {
+  const dropdownId = 'dropdown-' + category.name;
+  const changeVisibility = (visibility: 'visible' | 'collapsed') => {
+    const dropdown = document.getElementById(dropdownId);
+    if (!dropdown) return;
+    dropdown.style.visibility = visibility;
+  };
   return (
-    <div className='palette-dropdown'>
+    <div className='palette-dropdown' onClick={() => changeVisibility('visible')}>
       <div className='dropdown-title'>{category.name}</div>
-      <div className='dropdown-items'>
+      <div className='dropdown-items' id={dropdownId} onBlur={() => changeVisibility('collapsed')}>
         {category.items.map(item => (
           <PaletteItem key={item.name} item={item} />
         ))}
