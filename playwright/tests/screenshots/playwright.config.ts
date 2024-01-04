00841a3fcd2466d6e35f83f989a2c52ed698acf2
Lukas Lieb <lukas.lieb@axonivy.com>
2024-01-04 09:02:50 +0100

    XIVY-13128 Add playwright e2e test setup

---
+++ playwright/tests/screenshots/playwright.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, devices } from '@playwright/test';
+import defaultConfig from '../../playwright.base';
+
+export default defineConfig(defaultConfig, {
+  testDir: './',
+  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
+  webServer: {
+    command: 'npm run start:standalone',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI
+  }
+});
