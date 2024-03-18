f3f5dbef826baa16cb9c973fac341efcc483c278
Lukas Lieb <lukas.lieb@axonivy.com>
2024-03-18 08:16:58 +0100

    Update ui-components

--- packages/editor/src/components/editor/properties/fields/CheckboxField.tsx
+++ packages/editor/src/components/editor/properties/fields/CheckboxField.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Flex, Label, useFieldset } from '@axonivy/ui-components';
+import { Checkbox, Field, Label } from '@axonivy/ui-components';
 
 type CheckboxFieldProps = {
   label: string;
@@ -6,12 +6,9 @@ type CheckboxFieldProps = {
   onChange: (value: boolean) => void;
 };
 
-export const CheckboxField = ({ label, value, onChange }: CheckboxFieldProps) => {
-  const fieldset = useFieldset();
-  return (
-    <Flex alignItems='center' gap={1}>
-      <Checkbox checked={value} onCheckedChange={e => onChange(Boolean(e))} {...fieldset.inputProps} />
-      <Label {...fieldset.labelProps}>{label}</Label>
-    </Flex>
-  );
-};
+export const CheckboxField = ({ label, value, onChange }: CheckboxFieldProps) => (
+  <Field direction='row' alignItems='center' gap={1}>
+    <Checkbox checked={value} onCheckedChange={e => onChange(Boolean(e))} />
+    <Label>{label}</Label>
+  </Field>
+);
