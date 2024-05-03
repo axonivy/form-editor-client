0d3ac58368923712790a5cbd6ebb521140559b89
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-05-03 10:19:24 +0200

    XIVY-13820 introduce form-editor screenshots

--- playwright/tests/screenshots/playwright.config.ts
+++ playwright/tests/screenshots/playwright.config.ts
@@ -3,10 +3,19 @@ import defaultConfig from '../../playwright.base';
 
 export default defineConfig(defaultConfig, {
   testDir: './',
-  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 700, height: 1000},
+        contextOptions: { reducedMotion: 'reduce' },
+      }
+    }
+  ],
   webServer: {
     command: 'npm run start:standalone',
-    url: 'http://localhost:3000',
+    url: 'http://localhost:3000/mock.html',
     reuseExistingServer: !process.env.CI
   }
 });
--- playwright/tests/screenshots/screenshots.spec.ts
+++ playwright/tests/screenshots/screenshots.spec.ts
@@ -9,13 +9,20 @@ test.describe('screenshots', () => {
 
   test('form editor', async ({ page }) => {
     await FormEditor.open(page);
-    await screenshot(page, 'form-editor.png', { height: 350 });
+    await page.locator(".block-input").click();
+    await page.locator(".property-item").first().isVisible();
+    await screenshot(page, 'form-editor.png', { height: 350, width: 700 });
   });
 });
 
 async function screenshot(page: Page, name: string, size?: { width?: number; height?: number }) {
   await page.setViewportSize({ width: size?.width ?? 700, height: size?.height ?? 550 });
+  await hideQuery(page);
   const dir = process.env.SCREENSHOT_DIR ?? './target';
   const buffer = await page.screenshot({ path: `${dir}/screenshots/${name}`, animations: 'disabled' });
   expect(buffer.byteLength).toBeGreaterThan(3000);
+}
+
+async function hideQuery(page: Page) {
+  await page.addStyleTag({ content: `.tsqd-parent-container { display: none; }` });
 }
