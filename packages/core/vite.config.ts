0ecca51598fda3501808376d5c7b7e4b07727dfe
Louis <louis.mueller@axonivy.com>
2024-06-13 13:34:25 +0200

    package core using vite

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  esbuild: {
+    sourcemap: 'inline'
+  },
+  build: {
+    outDir: 'lib',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'core',
+      formats: ['es']
+    }
+  }
+});
