9d4abc064dfbcc72038359621d36e74475845512
Louis <louis.mueller@axonivy.com>
2024-06-14 10:53:46 +0200

    XIVY-14102 vite package core and editor to make it esm conform (#62)

--- packages/core/.eslintrc.cjs
+++ packages/core/.eslintrc.cjs
@@ -1,6 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['../../config/base.eslintrc.json'],
+  ignorePatterns: ['vite.config.ts'],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: 'tsconfig.json'
---
+++ packages/editor/vite.config.ts
@@ -0,0 +1,38 @@
+import { vanillaExtractPlugin as veVitePlugin } from '@vanilla-extract/vite-plugin';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [veVitePlugin(), visualizer(), react(), dts({ tsconfigPath: './tsconfig.production.json' })],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  esbuild: {
+    sourcemap: 'inline'
+  },
+  build: {
+    outDir: 'lib',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'editor',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: [
+        '@dnd-kit/core',
+        '@tanstack/react-query',
+        '@tanstack/react-query-devtools',
+        'react',
+        'react-error-boundary',
+        'react/jsx-runtime',
+        'react-dom',
+        'uuid'
+      ]
+    }
+  }
+});
