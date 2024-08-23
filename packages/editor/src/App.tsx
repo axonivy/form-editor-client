65c142bdefd15893ff1a7fce68ae0ddcdf6d2888
Lukas Lieb <lukas.lieb@axonivy.com>
2024-08-23 08:53:43 +0200

    Import package styles via css layer

--- packages/editor/src/App.tsx
+++ packages/editor/src/App.tsx
@@ -1,7 +1,6 @@
 import { Editor } from './editor/Editor';
-import '@axonivy/ui-icons/lib/ivy-icons.css';
-import '@axonivy/ui-components/lib/style.css';
 import type { FormEditorProps } from '@axonivy/form-editor-protocol';
+import './App.css';
 
 function App(props: FormEditorProps) {
   return <Editor {...props} />;
