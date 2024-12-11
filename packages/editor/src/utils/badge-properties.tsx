c57df9dde0fcfe6d67f65498b3155f4986f85e2b
Fabian Heuberger <fabian.heuberger@axonivy.com>
2024-12-11 07:44:54 +0100

    XIVY-15526 creates component for cmstooltip

--- packages/editor/src/utils/badge-properties.tsx
+++ packages/editor/src/utils/badge-properties.tsx
@@ -4,50 +4,11 @@ import { useMeta } from '../context/useMeta';
 import type { ContentObject } from '@axonivy/form-editor-protocol';
 import type { BadgeType } from '@axonivy/ui-components';
 
-const useCmsValues = (text: string) => {
-  const { context } = useAppContext();
-  const cmsItems = useMeta('meta/cms/cmsTree', { context, requiredProjects: false }, []).data;
-
-  const findCmsDeep = (targetPath: string, cmsTree: Array<ContentObject>) => {
-    const notFound = ['no cms entry found'];
-    if (cmsTree.length === 0) {
-      return notFound;
-    }
-    const entries = findCmsDeepRec(targetPath, cmsTree[0]);
-    if (!entries) {
-      return notFound;
-    }
-    return Object.entries(entries).map(key => `${key[0]}: ${key[1]}`);
-  };
-
-  const findCmsDeepRec = (targetPath: string, obj?: ContentObject) => {
-    if (!obj) return null;
-    if (obj.fullPath === targetPath) return obj.values;
-    if (!obj.children || obj.children.length === 0) return null;
-    return findCmsDeepRec(
-      targetPath,
-      obj.children.find(e => targetPath.startsWith(e.fullPath))
-    );
-  };
-
-  text = text.replaceAll(/#{ivy.cms.co\(|['"]|\)}/g, '');
-  const entries = findCmsDeep(text, cmsItems);
-  entries.unshift(text);
-  return entries.map((entry, i) => {
-    return (
-      <span key={i}>
-        {entry}
-        <br />
-      </span>
-    );
-  });
-};
-
 const badgePropertyCMS: BadgeType = {
   regex: /#{\s*ivy.cms.co[^}]+}/,
   icon: IvyIcons.Cms,
   badgeTextGen: (text: string) => '.../' + text.replaceAll(/#{\s*ivy.cms.co\(['"]|\/*[^\\]+\/|['"]\)\s*}/g, ''),
-  tooltip: useCmsValues
+  tooltip: (text: string) => <CmsTooltip text={text} />
 };
 
 const badgePropertyData: BadgeType = {
@@ -66,6 +27,45 @@ const badgePropertyExpression: BadgeType = {
   regex: /#{[^}]*}/,
   icon: IvyIcons.StartProgram,
   badgeTextGen: (text: string) => text.replaceAll(/#{\s*|\s*}/g, '')
+};
+
+const CmsTooltip = ({ text }: { text: string }) => {
+  const { context } = useAppContext();
+  const cmsItems = useMeta('meta/cms/cmsTree', { context, requiredProjects: false }, []).data;
+
+  const findCmsEntries = (targetPath: string, cmsTree: Array<ContentObject>) => {
+    const notFound = ['no cms entry found'];
+    if (cmsTree.length === 0) {
+      return notFound;
+    }
+    const entries = findDeep(targetPath, cmsTree[0]);
+    if (!entries) {
+      return notFound;
+    }
+    return Object.entries(entries).map(key => `${key[0]}: ${key[1]}`);
+  };
+
+  const findDeep = (targetPath: string, obj?: ContentObject) => {
+    if (!obj) return null;
+    if (obj.fullPath === targetPath) return obj.values;
+    if (!obj.children || obj.children.length === 0) return null;
+    return findDeep(
+      targetPath,
+      obj.children.find(e => targetPath.startsWith(e.fullPath))
+    );
+  };
+
+  text = text.replaceAll(/#{ivy.cms.co\(|['"]|\)}/g, '');
+  const entries = findCmsEntries(text, cmsItems);
+  entries.unshift(text);
+  return entries.map((entry, i) => {
+    return (
+      <span key={i}>
+        {entry}
+        <br />
+      </span>
+    );
+  });
 };
 
 export const badgeProps = [badgePropertyData, badgePropertyLogic, badgePropertyCMS, badgePropertyExpression];
