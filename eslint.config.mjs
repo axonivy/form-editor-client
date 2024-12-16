fa1a83600e31f39f837614a6574b66d267257c95
Lukas Lieb <lukas.lieb@axonivy.com>
2024-12-16 14:21:35 +0100

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,49 @@ import tseslint from 'typescript-eslint';
 import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
 import reactJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactCompiler from 'eslint-plugin-react-compiler';
+import reactCompiler, { react } from 'eslint-plugin-react-compiler';
+import globals from 'globals';
 
 export default tseslint.config(
   // Base eslint recommended config
   eslint.configs.recommended,
+
   // TypeScript recommended configs
   ...tseslint.configs.recommended,
   // ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true, // Uses tsconfig.json from current directory
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+
   // React recommended configs
   reactRecommended,
   reactJsxRuntime,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    rules: {
+      'react/display-name': 'off'
+    }
+  },
+
   // React Hooks plugin
   {
     plugins: {
@@ -23,6 +55,7 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules
     }
   },
+
   // React Compiler plugin
   {
     plugins: {
@@ -32,18 +65,8 @@ export default tseslint.config(
       'react-compiler/react-compiler': 'error'
     }
   },
-  // Project-specific overrides and custom rules
-  {
-    languageOptions: {
-      parserOptions: {
-        project: true, // Uses tsconfig.json from current directory
-        tsconfigRootDir: import.meta.dirname
-      }
-    },
-    rules: {
-      'react/display-name': 'off'
-    }
-  },
+
+  // Ignored files
   {
     ignores: [
       '**/node_modules/**',
@@ -55,5 +78,7 @@ export default tseslint.config(
       '**/schemaCodegen.cjs'
     ]
   },
+
+  // Project-specific overrides and custom rules
   { name: 'packages/core', files: ['packages/core/**/*.{js,mjs,cjs,ts,jsx,tsx}'], rules: { '@typescript-eslint/no-explicit-any': 'off' } }
 );
