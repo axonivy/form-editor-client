ff44d74941e61e9f129bd718291dbcddf772111d
Lukas Lieb <lukas.lieb@axonivy.com>
2025-01-06 11:34:49 +0100

    Use shared eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,13 +8,11 @@ import testingLibrary from 'eslint-plugin-testing-library';
 import playwright from 'eslint-plugin-playwright';
 import pluginQuery from '@tanstack/eslint-plugin-query';
 import globals from 'globals';
+import config from '@axonivy/eslint-config';
 
 export default tseslint.config(
-  // Base eslint recommended config
-  eslint.configs.recommended,
-
+  ...config.base,
   // TypeScript recommended configs
-  ...tseslint.configs.strict,
   {
     name: 'typescript-eslint',
     languageOptions: {
@@ -23,99 +21,6 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname
       }
     }
-  },
-
-  // React recommended configs
-  {
-    name: 'eslint-plugin-react/configs/recommended',
-    ...reactRecommended
-  },
-  {
-    name: 'eslint-plugin-react/configs/jsx-runtime',
-    ...reactJsxRuntime
-  },
-  {
-    name: 'eslint-plugin-react',
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        }
-      },
-      globals: {
-        ...globals.browser
-      }
-    },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    },
-    rules: {
-      'react/display-name': 'off'
-    }
-  },
-  {
-    name: 'eslint-plugin-react-hooks',
-    plugins: {
-      'react-hooks': reactHooks
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules
-    }
-  },
-  {
-    name: 'eslint-plugin-react-compiler',
-    plugins: {
-      'react-compiler': reactCompiler
-    },
-    rules: {
-      'react-compiler/react-compiler': 'error'
-    }
-  },
-
-  // Testing library recommended configs
-  {
-    name: 'eslint-plugin-testing-library',
-    files: ['**/*.test.{js,mjs,cjs,ts,jsx,tsx}'],
-    ...testingLibrary.configs['flat/react']
-  },
-
-  // Playwright recommended configs
-  {
-    name: 'eslint-plugin-playwright',
-    files: ['**/playwright/tests/**', '**/*.spec.{js,mjs,cjs,ts,jsx,tsx}'],
-    ...playwright.configs['flat/recommended'],
-    rules: {
-      ...playwright.configs['flat/recommended'].rules,
-      'playwright/expect-expect': 'off'
-    }
-  },
-
-  // Tanstack recommended configs
-  {
-    name: '@tanstack/eslint-plugin-query',
-    plugins: {
-      '@tanstack/query': pluginQuery
-    },
-    rules: {
-      ...pluginQuery.configs.recommended.rules
-    }
-  },
-
-  // Project-specific overrides and custom rules
-  {
-    name: 'ingored-files',
-    ignores: [
-      '**/node_modules/**',
-      'eslint.config.*',
-      '**/vite*.config.*',
-      '**/playwright.config.*',
-      '**/*.d.ts',
-      '**/lib/*',
-      '**/build/*',
-      '**/schemaCodegen.cjs'
-    ]
   },
   {
     name: 'packages/core',
