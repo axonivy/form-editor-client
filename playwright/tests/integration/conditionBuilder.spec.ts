fd2483bd49e5bc0a3e0fecef8d9c2c1967153740
Lukas Giger <lukas.giger@axonivy.com>
2025-04-28 09:56:00 +0200

    fix condition builder tests after browser tab change

--- playwright/tests/integration/conditionBuilder.spec.ts
+++ playwright/tests/integration/conditionBuilder.spec.ts
@@ -23,7 +23,7 @@ export async function applyConditionBuilder(page: Page) {
   const condition = page.locator('.ui-condition-builder-condition');
   const group = page.locator('.ui-condition-builder-group');
 
-  await expect(page.getByRole('dialog').getByRole('combobox').nth(0)).toHaveText('Basic Condition');
+  await page.getByRole('option').getByText('Basic Condition').click();
   await expect(condition).toHaveCount(1);
   await expect(condition.locator('output')).toHaveCount(2);
 
