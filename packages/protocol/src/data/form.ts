f62a5fc649887930defd5727c18ed416c54327fd
ivy-edp <elio.dipuma@axonivy.com>
2025-05-01 13:57:40 +0200

    XIVY-16228 Enhance Button Component for Flexible Styling - Add additional button variant options (success, info, etc.) - Enable switching between solid, outline and flat style - Add option to apply rounded edges to buttons

--- packages/protocol/src/data/form.ts
+++ packages/protocol/src/data/form.ts
@@ -10,8 +10,9 @@ export type CmsQuickactionCategory = ("global" | "local")
 export type FormType = "FORM" | "COMPONENT";
 export type LayoutAlignItems = "START" | "CENTER" | "END";
 export type ConfirmDialogSeverity = "INFO" | "WARN" | "ERROR" | "SUCCESS";
+export type ButtonStyle = "SOLID" | "OUTLINED" | "FLAT";
 export type ButtonType = "SUBMIT" | "BUTTON" | "RESET" | "EDIT" | "DELETE";
-export type ButtonVariant = "PRIMARY" | "SECONDARY" | "DANGER";
+export type ButtonVariant = "PRIMARY" | "SECONDARY" | "DANGER" | "SUCCESS" | "INFO" | "WARNING" | "HELP";
 export type SymbolPosition = "p" | "s";
 export type InputType = "TEXT" | "EMAIL" | "PASSWORD" | "NUMBER";
 export type LayoutGridVariant = "GRID1" | "GRID2" | "GRID4" | "FREE";
@@ -134,6 +135,8 @@ export interface Button {
   mdSpan: string;
   name: string;
   processOnlySelf: boolean;
+  rounded: boolean;
+  style: ButtonStyle;
   type: ButtonType;
   variant: ButtonVariant;
   visible: string;
