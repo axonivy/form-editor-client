6ddc7c7bf124f1dca3231a1ac06279aa8d172b9a
Lukas Lieb <lukas.lieb@axonivy.com>
2025-05-22 08:07:40 +0200

    Fix condition builder test

--- playwright/tests/integration/conditionBuilder.spec.ts
+++ playwright/tests/integration/conditionBuilder.spec.ts
@@ -7,22 +7,24 @@ test.describe('Condition Builder', () => {
     await editor.canvas.blockByNth(0).inscribe();
     await editor.inscription.expectHeader('Input');
     const properties = editor.inscription.section('Properties');
-    const behaviour = properties.behaviour();
+    const behaviour = properties.visibleBehaviour();
 
-    await page.getByText('Visible').click();
+    await behaviour.visibleField.focus();
     await behaviour.openConditionBuilder();
     await applyConditionBuilder(page);
-    await page.getByText('Visible').click();
+    await behaviour.visibleField.focus();
     await behaviour.expectVisibleAfterBuilder();
   });
 });
 
 export async function applyConditionBuilder(page: Page) {
-  await expect(page.getByRole('dialog')).toBeVisible();
-  await page.getByText('Condition').first().click();
-  const condition = page.locator('.ui-condition-builder-condition');
-  const group = page.locator('.ui-condition-builder-group');
+  const builder = page.getByRole('dialog');
+  const select = builder.getByRole('combobox').nth(0);
+  const condition = builder.locator('.ui-condition-builder-condition');
+  const group = builder.locator('.ui-condition-builder-group');
 
+  await expect(builder).toBeVisible();
+  await select.click();
   await page.getByRole('option').getByText('Basic Condition').click();
   await expect(condition).toHaveCount(1);
   await expect(condition.locator('output')).toHaveCount(2);
@@ -34,7 +36,7 @@ export async function applyConditionBuilder(page: Page) {
   await condition.first().locator('output').nth(1).click();
   await condition.locator('.ui-input').nth(0).fill('10');
 
-  await page.getByLabel('Add Condition').click();
+  await builder.getByLabel('Add Condition').click();
   await expect(condition).toHaveCount(2);
   await condition.nth(1).locator('output').nth(0).click();
   await condition.nth(1).locator('.ui-input').nth(0).fill('data.value2');
@@ -43,21 +45,21 @@ export async function applyConditionBuilder(page: Page) {
   await condition.nth(1).locator('output').nth(1).click();
   await condition.nth(1).locator('.ui-input').nth(0).fill('5');
 
-  await page.getByRole('dialog').getByRole('combobox').nth(0).click();
+  await select.click();
   await page.getByRole('option', { name: 'Nested Condition', exact: true }).first().click();
 
   await expect(group).toHaveCount(1);
-  await page.getByLabel('Add Condition Group').click();
+  await builder.getByLabel('Add Condition Group').click();
   await expect(group).toHaveCount(2);
-  await page.locator('.ui-dialog-content').getByRole('combobox').nth(4).click();
+  await builder.getByRole('combobox').nth(4).click();
   await page.getByRole('option', { name: 'or', exact: true }).first().click();
 
   await condition.nth(2).locator('output').nth(0).click();
   await condition.nth(2).locator('.ui-input').nth(0).fill('data.value3');
-  await page.locator('.ui-condition-builder-condition').nth(2).getByRole('combobox').click();
+  await condition.nth(2).getByRole('combobox').click();
   await page.getByRole('option', { name: 'less than', exact: true }).first().click();
   await condition.nth(2).locator('output').nth(1).click();
   await condition.nth(2).locator('.ui-input').nth(0).fill('6');
 
-  await page.getByRole('button', { name: 'Apply' }).click();
+  await builder.getByRole('button', { name: 'Apply' }).click();
 }
